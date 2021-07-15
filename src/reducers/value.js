cd0b5d48a39649abb4175a9aabebe9a2beb4ca52
Ilya Melishnikov <364844@gmail.com>
2021-07-15 17:37:14 +0300

    add ability to set default value from existing values in block

--- src/reducers/value.js
+++ src/reducers/value.js
@@ -3,6 +3,14 @@ import getPath from 'lodash/get'
 import { createFieldReducer } from '../createFieldReducer'
 
 const value = (defaultValue) =>
-  createFieldReducer((v, _, path) => getPath(v, path, defaultValue))
+  createFieldReducer((v, _, path) => {
+    const fieldValue = getPath(v, path)
+    if (typeof fieldValue === 'undefined' && typeof defaultValue === 'function') {
+      return defaultValue(v)
+    }
+
+
+    return getPath(v, path, defaultValue)
+  })
 
 export default value
