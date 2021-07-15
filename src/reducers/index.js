9965d6f165cbf8b9d735d4e95f2a91937d557bdd
Ilya Melishnikov <364844@gmail.com>
2021-07-15 17:38:21 +0300

    add export shortcut for reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,5 @@
+export { default as fields } from './fields'
+export { default as memo } from './memo'
+export { default as option } from './option'
+export { default as reduce } from './reduce'
 export { default as value } from './value'
